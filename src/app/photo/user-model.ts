961c2fd48d5cbaf4089220573a98539ef3155f65
rosedavid <roselinetandom@gmail.com>
2019-04-10 10:40:26 +0800

    Version 1

---
+++ src/app/photo/user-model.ts
@@ -0,0 +1,19 @@
+import {Address} from './address-model';
+import {Company} from './company-model';
+
+export interface User {
+
+  Id: number,
+  name: string,
+  username: string,
+  email: string,
+  address: Address,
+  phone: string,
+  website: string,
+  company: Company
+
+}
+
+
+
+
